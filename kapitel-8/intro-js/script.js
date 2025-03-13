bd80d730fa608ee7ec7f7d027683b25f1ebcabc9
Romeo Calderon <romeo.calderon@elev.ga.ntig.se>
2025-03-13 14:26:47 +0100

    Tränat på javascript, variabler, konkatenering

---
+++ kapitel-8/intro-js/script.js
@@ -0,0 +1,18 @@
+// var tal1 = 12;
+// var tal2 = 34;
+// var summa = tal1 + tal2;
+// alert(tal1 + " + " + tal2 + " = " + summa);
+
+var förnamn = "Romeo";
+var efternamn = "Calderon";
+var ålder = 18;
+var namn = förnamn + " " + efternamn;
+// var meddelande = namn + " är " + ålder + " år gammal.";
+// alert(meddelande);
+
+var hobby = "kampsport"
+var kostnad = 150;
+var meddelande = namn + " gillar " + hobby + " \noch" + " det" + " kostar" + kostnad + " per" + " år";
+
+// Tex Karim Ryde gillar segling och det kostar 10 per år
+alert(meddelande);
